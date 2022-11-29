39f48abecb2682b1d58565a3ef7303aeb42317d7
heayriwang <118506446+heayriwang@users.noreply.github.com>
2022-11-29 17:09:28 +0900

    22.11.29

---
+++ js/graff.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    //topBanner
+    $('.topBanner i').on('click', function () {
+        $('.topBanner').addClass('on')
+    });
+
+    // mainSlide
+    $('.mainSlide').slick({
+        // slidesToShow: 2,
+        //arrows:false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false,
+        pauseOnFocus: false
+    });
+
+
+
+
+
+
+
+});
