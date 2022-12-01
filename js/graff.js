3943bacaad880bb87f12677765419826f0ad9dae
heayriwang <118506446+heayriwang@users.noreply.github.com>
2022-12-01 17:46:47 +0900

    22.12.01

--- js/graff.js
+++ js/graff.js
@@ -42,5 +42,52 @@ $(function () {
         $('.main_pro_slide').slick('slickNext')
     });
 
+    //tab menu
+
+    $('.mainCustomer .tab_menu>li').on('click', function (event) {
+        event.preventDefault();
+        var _this = $(this); //이벤트가 적용된 자신을 반환
+        var idx = $(this).index(); //이벤트가 적용된 자신의 번호를 반환 0,1,2
+        $(this).addClass('on').siblings().removeClass('on')
+        $('.mainCustomer .tab_content>li').eq(idx).addClass('on').siblings().removeClass('on')
+    });
+
+    $('#f_link').on('change', function () {
+        var link = $(this).val();
+        link && window.open(link);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
